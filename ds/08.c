63eac10659f79804d363eefbde2078302cc434e7
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-03 20:43:22 +0530

    Update 08.c

--- ds/08.c
+++ ds/08.c
@@ -1,54 +1,3 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     int m;    
-//     int i=0,j=0;
-
-//     printf("enter the element of arr a");
-//     scanf("%d",&n);
-//     int arr1[n];
-//     printf("enter the elements");
-//     for (int a=0;a<n;a++){
-//         scanf("%d",&arr1[a]);
-//     }
-//     printf("enter the no of element");
-//     scanf("%d",&m);
-//     int arr2[m];
-//     int c[m+n];
-//     printf("enter the element of arr2");
-//     for (int b=0;b<m;b++){
-//         scanf("%d",&arr2[b]);
-//     }
-//     int k=0;
-//     while (i<n && j<m){
-//         if (arr1[i]<arr2[j]){
-//             c[k++]=arr1[i++];
-           
-//         }
-//         if (arr1[i]>arr2[j]){
-//             c[k++]=arr2[j++];
-//         }
-//         if (arr1[i]=arr2[j]){
-//             c[k++]=arr2[j++];
-//             c[k++]=arr1[i++];
-//         }
-//     }
-//     while (i<n)
-//     {
-//         c[k++]=arr1[i++];
-//     }
-//     while (j<m)
-//     {
-//         c[k++]=arr2[j++];
-//     }
-    
-    
-//     for (int k=0;k<(m+n);k++){
-//         printf("%d ",c[k]);
-//     }
-//     return 0;
-// }
-
 #include<stdio.h>
 
 int main(){
@@ -66,7 +15,6 @@ int main(){
     }
     int c[n1+n2];
     int k=0;
-
     for (int j=0;j<n1;j++){
         c[k++]=a[j];
 
