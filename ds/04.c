a0d47efee5ca9600a812b15ea1711e2a9c6c5bbd
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-03 20:39:49 +0530

    Update 04.c

--- ds/04.c
+++ ds/04.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 int main(){
     int n;
     scanf("%d",&n);
@@ -21,9 +20,6 @@ int main(){
     }
     for (int i=0;i<n-1;i++){
         printf("%d",arr[i]);
-    }
-        
-
-    
+    }   
     return 0;
 }
