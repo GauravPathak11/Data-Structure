1852d2f06851d24a87a2dfb44e24eed61977d950
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-07 13:20:44 +0530

    Update boolean algebra (1).c

--- ds/boolean algebra (1).c
+++ ds/boolean algebra (1).c
@@ -1,13 +1,10 @@
-
 #include<stdio.h>
 int main(){
-
 int i;
 int a[4]={1,0,0,1};
 int b[4]={0,1,0,1};
 printf("A B");
 for(i=0;i<4;i++){
-
     printf("\n%d%d\n",a[i],b[i]);
 }
 for(i=0;i<4;i++){
@@ -16,7 +13,6 @@ for(i=0;i<4;i++){
 printf("AND GATE");
 for(i=0;i<4;i++){
     printf("\n%d\n",(a[i] & b[i]));
-
 }
 printf(" OR gate");
 for(i=0;i<4;i++){
@@ -25,12 +21,9 @@ for(i=0;i<4;i++){
 printf("Not gate\n");
 for(i=0;i<4;i++){
     printf("\n%d%d\n",!a[i],!b[i]);
-}printf("");
-
-
-
-
-
+}
+    printf("");
+    return 0;
 }
 
 
