e4e09b9f148498c1f1b93c601cdb9d573cd56f21
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-07 13:19:36 +0530

    Update 21.c

--- ds/21.c
+++ ds/21.c
@@ -1,20 +1,13 @@
-
-
-
-
 #include<stdio.h>
-
 int main(){
     int n,m;
     scanf("%d %d",&n,&m);
     int a[n][m];
-
     printf("enter a elements");
     for (int i=0;i<n;i++){
         for (int j=0;j<m;j++){
             scanf("%d",&a[i][j]);
         }
-
     }
     for (int i=0;i<n;i++){
         for (int j=0;j<m;j++){
@@ -22,16 +15,13 @@ int main(){
         }
 printf("\n");
     }
-
 printf("\n");
     for (int i=0;i<n;i++){
         for (int j=0;j<i;j++){
             int temp;
             temp=a[i][j];
             a[i][j]=a[j][i];
-            a[j][i]=temp;
-
-            
+            a[j][i]=temp;            
         }
     }
     for (int i=0;i<m;i++){
