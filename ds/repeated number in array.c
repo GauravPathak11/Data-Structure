94cf39cf664951b95d06cdb6e0f70cda293cd2a1
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-10 22:01:36 +0530

    Update repeated number in array.c

--- ds/repeated number in array.c
+++ ds/repeated number in array.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 void main(){
-
 int arr[]={1,2,3,4,5,6,8,8,3};
 int length=sizeof(arr)/sizeof(arr[0]);
-
 int i,j;
 printf("repeated element in given array\n");
 for(i=0;i<length;i++){
@@ -13,8 +11,5 @@ for(i=0;i<length;i++){
             printf("%d",arr[j]);
     }
 }
-
-
 return 0;
-
 }
