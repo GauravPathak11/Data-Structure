31fc86bf6c7d5cc6495bdfb909e8cf5a51fac5c4
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-10 22:02:13 +0530

    Update merging of two array.c

--- ds/merging of two array.c
+++ ds/merging of two array.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main(){
  int a[50],b[50],i,j,n,c[50];
-
  printf("Enter the number of elemnt a\n");
  scanf("%d",&n);
  for(i=0;i<n;i++){
@@ -12,12 +11,9 @@ int main(){
      if(a[i]=c||b[i]=c){
         printf("The merging of the two array\");
      scanf("%d",&c[i]);
-
     }
     }
     printf("\n");
-
-
  }
 
 
