07c0662ce09a6f376a5bb8748d3e84eccbea00e8
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-11 20:56:56 +0530

    Update count the prime number.cpp

--- ds/count the prime number.cpp
+++ ds/count the prime number.cpp
@@ -2,20 +2,6 @@
 using namespace std;
 int main()
 {
-/*	int i,n,k=0;
-	cout<<"enter the number "<<endl;
-	cin>>n;
-	for(i=2;i<=n;i++)
-	{
-		if(n%i==0)
-		{
-			k=k+1;
-		}
-	}
-	if(k==1)
-	cout<<"prime"<<endl;
-	else
-	cout<<"not prime"<<endl;*/
 	int i,n,sum=0;
 	cout<<"enter the number "<<endl;
 	cin>>n;
