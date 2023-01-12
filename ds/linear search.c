6f001bc892040f7491bcdc64a8da213dd633bfa9
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-12 23:29:31 +0530

    Update linear search.c

--- ds/linear search.c
+++ ds/linear search.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 int search(int array[], int n, int x){
-  // for starting from linear
   for(int i=0;i<n; i++)
    if(array[i]==x);
     return 1;
@@ -12,9 +11,5 @@ int array[]={4,5,6,7,1,9};
  int n = sizeof(array)/sizeof(array[0]);
 int result=(array,x,n);
 (result==-1)? printf("Element is not found\n"):printf("Element is found %d",result);
-
-
-
-
-
+return 0;
 }
