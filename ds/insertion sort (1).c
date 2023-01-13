b45b5eeef430b6869e630122da730fd6f2d96d85
Gaurav Pathak <121251982+pathakgaurav11@users.noreply.github.com>
2023-01-13 19:47:27 +0530

    Update insertion sort (1).c

--- ds/insertion sort (1).c
+++ ds/insertion sort (1).c
@@ -20,16 +20,12 @@ void printArray(int arr[], int n)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
-
 int main()
 {
     int arr[] = { 12, 11, 13, 5, 6 };
     int n = sizeof(arr) / sizeof(arr[0]);
-
     insertionSort(arr, n);
     printArray(arr, n);
-
     return 0;
 }
 
